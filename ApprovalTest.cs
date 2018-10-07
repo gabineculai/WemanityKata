dee1a52ca543e67bf32a6687c954cdbd04af02c7
gabineculai <43930760+gabineculai@users.noreply.github.com>
2018-10-07 15:51:36 +0200

    Add files via upload

---
+++ ApprovalTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace csharp
+{
+    [TestFixture]
+    public class ApprovalTest
+    {
+        [Test]
+        public void ThirtyDays()
+        {
+            var lines = File.ReadAllLines("ThirtyDays.txt");
+
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+
+            string[] stringSeparators = new string[] { "\r\n" };
+            var outputLines = output.Split(stringSeparators, StringSplitOptions.None);
+            for(var i = 0; i<Math.Min(lines.Length, outputLines.Length); i++) 
+            {
+                Assert.AreEqual(lines[i], outputLines[i]);
+            }
+        }
+    }
+}
