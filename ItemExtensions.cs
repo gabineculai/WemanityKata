0f23c6dcf354d6b0793be749660dc3851af64edf
gabineculai <43930760+gabineculai@users.noreply.github.com>
2018-10-07 16:03:01 +0200

    Add files via upload

---
+++ ItemExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public static class ItemExtensions
+    {
+        public static void UpdateQuality(this Item item)
+        {
+            QualityUpdaterFactory.GetByItem(item).UpdateItemQuality(item);
+        }
+    }
+}
